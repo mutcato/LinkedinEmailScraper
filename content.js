283893f7c2148899f41db31603626257c46e3780
mutcato <mtokat1@gmail.com>
2019-03-17 12:22:39 +0300

    hesap gümleten content.js

--- content.js
+++ content.js
@@ -1,20 +1,79 @@
 console.log("content matched");
 
-function getElementByXpath(path) {
-    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
-
-let art_deco = document.getElementsByClassName("artdeco-card");
-function get_connections(){
-    let list_items = $("li.list-style-none .mn-connection-card__picture").map( function() {
-        return $(this).attr('href');
+function get_names(){
+    let art_deco = document.getElementsByClassName("artdeco-card");
+    let names = $(".mn-connection-card__name").map( function() {
+        return $(this).text().trim();
     }).get();
-    return list_items;
+    return names;
 }
-console.log(get_connections());
 
-$(window).scroll(function () {
-    if ($(document).height()*0.9 <= $(window).scrollTop() + $(window).height()) {
-        console.log(get_connections());
+function get_usernames(){
+    let art_deco = document.getElementsByClassName("artdeco-card");
+    let usernames = $("li.list-style-none .mn-connection-card__picture").map( function() {
+        let href = $(this).attr('href');
+        return href.substring(href.lastIndexOf("/in/") + 4, href.lastIndexOf("/"));
+    }).get();
+    return usernames;
+}
+
+function get_connections(usernames){
+    /**
+     * 
+     * @param {array} usernames 
+     * @return {array} emails
+     */
+    let names = get_names();
+    let connections = [];
+    for (let i=0; i<usernames.length; i++){
+        // Get email addresses
+        $.ajax({ 
+            url: 'https://www.linkedin.com/in/'+usernames[i]+'/detail/contact-info/', 
+            success: function(data) {
+                $htmlData = data;
+                $email = $htmlData.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi); 
+                connections.push({name: names[i], email: $email[0], username: usernames[i]}); 
+            } });
+
     }
+    return connections;
+}
+
+let usernames = get_usernames();
+let connections = get_connections(usernames);
+
+//make room for salaries section
+$(".mn-connection-card").css("margin-left", "100px");
+
+console.log(connections);
+//list_items'i db'ye kaydet.
+
+/*
+var controll = 1; 
+$(window).scroll(function () {
+        let usernames = get_usernames();
+        //Her scrollda get_connections() fonksiyonunun çalışması hesabı kitliyor.
+        //let new_list = get_connections(usernames);
+        console.log("new List:");
+        console.log(new_list.length);
+        console.log("List Items:");
+        console.log(list_items.length);  
+        $(".mn-connection-card").css("margin-left", "100px");
+
+        if(new_list > list_items){
+                controll = 0;
+                // pick connections is in new_list but not in list_items(old list)
+                let difference_list = new_list.filter(x => !list_items.includes(x));
+                //difference_list'i db'ye kaydet.
+                list_items = new_list;
+                console.log(difference_list);                
+
+        }else{
+                console.log("pop");
+                controll = 1;
+                //çıkanların profil ücret işlemelerini yap                
+
+        }
+        
 });
+*/
