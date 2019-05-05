5603121cb2316ac0b1cc2d238f224aab2ef2cbc5
mutcato <mtokat1@gmail.com>
2019-05-06 01:24:55 +0300

    sayfa yenilenmeden tıklamalarda insert yapıyor.

--- background.js
+++ background.js
@@ -1,2 +1,15 @@
 console.log("heyy i am background.js")
 
+chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+      // read changeInfo data and do something with it
+      // like send the new url to contentscripts.js
+      if (changeInfo.url) {
+        console.log(changeInfo.url);
+        chrome.tabs.sendMessage( tabId, {
+          message: 'url_changed!',
+          url: changeInfo.url
+        })
+      }
+    }
+  );
--- content.js
+++ content.js
@@ -1,79 +1,59 @@
-console.log("content matched");
+console.log("content matched!!!");
 
-function get_names(){
-    let art_deco = document.getElementsByClassName("artdeco-card");
-    let names = $(".mn-connection-card__name").map( function() {
-        return $(this).text().trim();
-    }).get();
-    return names;
+// function get_names(){
+//     let art_deco = document.getElementsByClassName("artdeco-card");
+//     let names = $(".mn-connection-card__name").map( function() {
+//         return $(this).text().trim();
+//     }).get();
+//     return names;
+// }
+
+function get_victimname(url){
+    return url.substring(url.lastIndexOf("/in/") + 4, url.lastIndexOf("/"));
 }
 
-function get_usernames(){
-    let art_deco = document.getElementsByClassName("artdeco-card");
-    let usernames = $("li.list-style-none .mn-connection-card__picture").map( function() {
-        let href = $(this).attr('href');
-        return href.substring(href.lastIndexOf("/in/") + 4, href.lastIndexOf("/"));
-    }).get();
-    return usernames;
-}
+console.log(get_victimname(window.location.href));
 
-function get_connections(usernames){
-    /**
-     * 
-     * @param {array} usernames 
-     * @return {array} emails
-     */
-    let names = get_names();
-    let connections = [];
-    for (let i=0; i<usernames.length; i++){
-        // Get email addresses
+function get_victim_email(victim_name){
+    $.ajax({ 
+    url: 'https://www.linkedin.com/in/'+victim_name+'/detail/contact-info/', 
+    success: function(data) {
+        $htmlData = data;
+        $email = $htmlData.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi); 
+        if($email){
+           console.log($email[0]);  
+        }      
+    } });
+}
+get_victim_email(get_victimname(window.location.href));
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      // listen for messages sent from background.js
+      if (request.message === 'url_changed!') {
+        console.log(request.url) // new url is now in content scripts!
+        var victim_name = get_victimname(request.url);
+        console.log(victim_name);
         $.ajax({ 
-            url: 'https://www.linkedin.com/in/'+usernames[i]+'/detail/contact-info/', 
+            url: 'https://www.linkedin.com/in/'+victim_name+'/detail/contact-info/', 
             success: function(data) {
                 $htmlData = data;
                 $email = $htmlData.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi); 
-                connections.push({name: names[i], email: $email[0], username: usernames[i]}); 
+                if($email){
+                    var postdata_arr = [];
+                    postdata_arr.push({username:victim_name,email:$email[0]});
+                   console.log(postdata_arr); 
+                   $.post("https://banabenianlat.net/ChromeExtensions/LinkedinScraper/create_email.php",
+                   {
+                       postdata:postdata_arr
+                   },
+                   function(data, status){
+                       console.log(status);
+                        console.log(" return data : "+data);
+                   });
+
+                }      
             } });
+      }
+  });
 
-    }
-    return connections;
-}
-
-let usernames = get_usernames();
-let connections = get_connections(usernames);
-
-//make room for salaries section
-$(".mn-connection-card").css("margin-left", "100px");
-
-console.log(connections);
-//list_items'i db'ye kaydet.
-
-/*
-var controll = 1; 
-$(window).scroll(function () {
-        let usernames = get_usernames();
-        //Her scrollda get_connections() fonksiyonunun çalışması hesabı kitliyor.
-        //let new_list = get_connections(usernames);
-        console.log("new List:");
-        console.log(new_list.length);
-        console.log("List Items:");
-        console.log(list_items.length);  
-        $(".mn-connection-card").css("margin-left", "100px");
-
-        if(new_list > list_items){
-                controll = 0;
-                // pick connections is in new_list but not in list_items(old list)
-                let difference_list = new_list.filter(x => !list_items.includes(x));
-                //difference_list'i db'ye kaydet.
-                list_items = new_list;
-                console.log(difference_list);                
-
-        }else{
-                console.log("pop");
-                controll = 1;
-                //çıkanların profil ücret işlemelerini yap                
-
-        }
-        
-});
-*/
